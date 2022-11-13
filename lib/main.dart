77c61b53f69883efbc3c1ca5581c5ad44dedfff0
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2022-11-13 22:26:27 +0100

    Configurar o projeto Uber

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:uber/tela/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Uber",
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
---
+++ lib/tela/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _nameState();
+}
+
+class _nameState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
